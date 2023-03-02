1924b531c68c70c783d488af0d43dcb72d36b7a9
Joshua Dela Cruz <delacruz.joshua.dev@gmail.com>
2023-03-02 20:40:03 +0800

    feature/community UI

--- src/pages/community.tsx
+++ src/pages/community.tsx
@@ -1,22 +1,38 @@
-import { type ReactElement } from "react";
+import { useState, type ReactElement } from "react";
 import { type NextPageWithLayout } from "./_app";
 
+import friends from "@data/friends.json";
 import MainLayout from "@layouts/main-layout";
+import UserFilter from "@features/community/user-filter";
+import UserListItem from "@features/community/user-list-tem";
+
+const filterOptions = [
+  { label: "Followers", value: "followers" },
+  { label: "Following", value: "following" },
+  { label: "You might know", value: "you-might-know" },
+];
 
 const Community: NextPageWithLayout = () => {
+  const [filter, setFilter] = useState("followers");
   return (
     <div className="grid grid-rows-[auto_1fr] p-6">
-      <header className="grid grid-cols-3 gap-4 rounded-lg bg-white p-4 text-gray-700">
-        <button className="h-full rounded-lg border-2 p-2 px-4 font-semibold transition-all duration-100 ease-linear hover:bg-blue-400 hover:text-white">
-          52,333 Followers
-        </button>
-        <button className="h-full rounded-lg border-2 p-2 px-4 font-semibold transition-all duration-100 ease-linear hover:bg-blue-400 hover:text-white">
-          2,433 Following
-        </button>
-        <button className="h-full rounded-lg border-2 p-2 px-4 font-semibold transition-all duration-100 ease-linear hover:bg-blue-400 hover:text-white">
-          People You might like
-        </button>
+      <header className="rounded-lg bg-white p-4">
+        <UserFilter
+          filterOptions={filterOptions}
+          currentValue={filter}
+          selectFilter={setFilter}
+        />
       </header>
+      <div className="mt-4 grid grid-cols-3 gap-4">
+        {friends?.map((friend) => (
+          <UserListItem
+            key={friend.id}
+            name={friend.name}
+            userId={friend.userId}
+            isFriend={friend.isFriend}
+          />
+        ))}
+      </div>
     </div>
   );
 };
